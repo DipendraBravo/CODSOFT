9d4450fb39568093a48c2188952f03d0d704153c
dipendra <dipendra.tamang9@gmail.com>
2023-07-03 11:54:47 +0545

    guessing number game

---
+++ NumberGuessingGame.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+
+public class NumberGuessingGame {
+
+    public void playGame(){
+        System.out.println("Welcome to Number Guessing Game");
+        System.out.println("you can try upto 5 times");
+        int max=100;
+        int min=1;
+        int targetNumber = (int)(Math.random()*(max-min+1)+min);
+        System.out.println(targetNumber);
+        int attempts=0;
+
+        while(true){
+            System.out.println("please enter the number from 1 to 100");
+            Scanner scan = new Scanner(System.in);
+            int guessNumber = scan.nextInt();
+            attempts++;
+            if (attempts<=5){
+                    if(guessNumber == targetNumber){
+                        System.out.println("Congratulations ! you guessed correct number in "+attempts+"attempts");
+                        break;
+                    }else if(guessNumber>targetNumber){
+                        System.out.println("Too high . Better luck next time");
+                        break;
+                    }
+                    else {
+                        System.out.println("Too low. Better luck next time");
+                        break;
+                    }
+            }
+            break;
+        }
+        playAgain();
+    }
+
+    public void playAgain(){
+        System.out.println("Do you want to play again?(yes/no)");
+        Scanner scan = new Scanner(System.in);
+        String choice = scan.next();
+        if(choice.equalsIgnoreCase("yes")){
+            playGame();
+        }
+        else {
+            System.out.println("Thank you so much for participating in Game");
+        }
+    }
+    public static void main(String[] args) {
+        NumberGuessingGame object = new NumberGuessingGame();
+        object.playGame();
+    }
+}
